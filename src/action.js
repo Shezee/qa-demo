fecb1a13aacd52494136de2acc2757ffbfc76e4b
Shazeen Mahmood <mahmood@talon.one>
2023-03-06 13:21:17 +0100

    comment on PR

--- src/action.js
+++ src/action.js
@@ -1,6 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
 
   console.log('Thank you QA team');
+
+  await octokit.rest.issues.createComment({
+
+    ...context.repo,
+
+    issue_number: pull_request.number,
+
+    body: `Thank you for submitting a pull request! We will try to review this as soon as we can.`
+
+  });
  
  }
  
